3867f155a5e89a998041fe382b6077ab43d60975
Esther <yaojingshuihudie@163.com>
2014-11-02 21:31:18 +0800

    main.js

---
+++ stylesheets/main.js
@@ -0,0 +1,35 @@
+body {
+	background:#7de2ff;
+	min-height:0
+}
+.container {
+	margin:0 auto;
+	height: 680px;
+	width:100%;
+	position:relative;
+	background:url(../images/clouds.png) center top no-repeat
+}
+.dialog {
+	background:url(../images/dialog.png) no-repeat;
+	width:425px;
+	height:190px;
+	position:absolute;
+	bottom:166px;
+	left:206px;
+	z-index:3000;
+}
+.dialog .text{
+	margin:50px 50px 0 86px;
+	font-size:14px;
+	line-height:20px
+}
+.dialog .text strong {
+	font-size:18px
+}
+.city {
+	background:url(../images/city.png);
+	width:100%;
+	height:93px;
+	position:fixed;
+	bottom:0
+}
